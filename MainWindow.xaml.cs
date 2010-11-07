6d711a1b2faa8a8f92f6e761a201a595243ec3e4
Cev <doubletime@gmail.com>
2010-11-07 02:52:00 -0500

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace FlashPatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string filePath;
+        static string safeFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileSelect_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                filePath = ofd.FileName;
+                safeFilePath = ofd.SafeFileName;
+
+                filename.Content = safeFilePath;
+            }
+        }
+
+        private void apply_Click(object sender, RoutedEventArgs e)
+        {
+            if (filePath != null)
+            {
+                bool patchResponse = false;
+                try
+                {
+                    patchResponse = Patch.apply(filePath);
+                    if (!patchResponse)
+                    {
+                        string messageBoxText = "Error patching file: Could not find location to patch";
+                        string caption = "Patch Failed";
+                        MessageBoxButton button = MessageBoxButton.OK;
+                        MessageBoxImage icon = MessageBoxImage.Error;
+                        MessageBox.Show(messageBoxText, caption, button, icon);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string messageBoxText = ex.Message;
+                    string caption = "Patch Success";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Error;
+                    MessageBox.Show(messageBoxText, caption, button, icon);
+                }
+
+                if (patchResponse)
+                {
+                    string messageBoxText = "Patch sucessfully applied! Restart your browser for the changes to take effect.";
+                    string caption = "Patch Success";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Exclamation;
+                    MessageBox.Show(messageBoxText, caption, button, icon);
+                }
+            }
+            else
+            {
+                string messageBoxText = "Error: No file selected";
+                string caption = "Error";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show(messageBoxText, caption, button, icon);
+            }
+
+
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
---
+++ Patch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace FlashPatch
+{
+    class Patch
+    {
+        public static bool apply(string filename)
+        {
+
+            var searchString = new Byte[] {0x74, 0x39, 0x83, 0xe8};
+            var buff = new Byte[] { 0x90, 0x90 };
+
+            var bytes = File.ReadAllBytes(filename);
+            var location = -1;
+
+            // Get the first location where file data matches search bytes
+            // please forgive algorithm (or lack thereof), this was written at 2am
+            bool found = false;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                for (int j = 0; j < searchString.Length; j++)
+                {
+                    found = true;
+                    if (bytes[i + j] != searchString[j])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    location = i;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                return false;
+            }
+
+            // Backup
+            string backupFilename = filename + "_backup";
+            File.Delete(backupFilename);
+            File.Copy(filename, backupFilename);
+
+            writeBytes(location, buff, filename);
+
+            return true;
+        }
+
+        private static void writeBytes(int location, byte[] buff, string fileName)
+        {
+            var fs = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
+            fs.Position = location;
+
+            foreach (var b in buff)
+            {
+                fs.WriteByte(b);
+            }
+           
+            fs.Close();
+        }
+
+
+    }
+}
